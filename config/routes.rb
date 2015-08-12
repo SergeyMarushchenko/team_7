371641f1eecc37ba145f90ef4559a8261f39e2f6
SergeyMarushchenko <marushchenkos@yandex.ru>
2015-08-12 23:11:00 +0300

    merged with vlad

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-=======
   devise_for :users
->>>>>>> 7948c71bee35145ffebb5c6ad19f57693eaf4d38
   #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'my_first_page#index'
 
